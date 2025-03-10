b3d1248e593eef078d549a570a84ccf565893eac
Erykah <erkahwanga@gmail.com>
2025-03-10 12:05:23 +0300

    Add function expressions and arrow functions for basic arithmetic operations

---
+++ index.js
@@ -0,0 +1,11 @@
+// Function expression
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow function with implicit return
+  const square = x => x * x;
+  
+  // Arrow function with multiple parameters
+  const add = (a, b) => a + b;
+  
